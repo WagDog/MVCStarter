a6fe9b457a2c20e6b2f173d268a7ebad87ad1092
Paul Wagstaff <paulwagstaff66@gmail.com>
2016-11-02 16:27:29 +0000

    Initial Update 2016-11-02

---
+++ MVCStarter/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using MVCStarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStarter.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            ViewBag.SomeMessage = "This is a message from the controller";
+            List<Product> products = new List<Product>();
+            products.Add(new Product() { ProductID = 1, ProductName = "Product 1" });
+            products.Add(new Product() { ProductID = 2, ProductName = "Product 2" });
+            products.Add(new Product() { ProductID = 3, ProductName = "Product 3" });
+
+            return View(products);
+        }
+    }
+}
